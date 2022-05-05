7b53e5c800c6d74c20c0684d86dae7b47250d487
brandon <srlguitarist@gmail.com>
2022-05-04 23:05:48 -0400

    Move file

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -1,14 +1,14 @@
 /* hide header when scrolling down */
 
 let prevScrollPos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
   if (currentScrollPos > 100) {
     if (prevScrollPos > currentScrollPos) {
-        document.querySelector("header").style.top = "0";
+      document.querySelector("header").style.top = "0";
     } else {
-        document.querySelector("header").style.top = "-200px";
+      document.querySelector("header").style.top = "-200px";
     }
   }
   prevScrollPos = currentScrollPos;
-}
+};
