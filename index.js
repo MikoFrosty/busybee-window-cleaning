ad0e1a7d849804908d12d92c64b63591f2f9e395
brandon <srlguitarist@gmail.com>
2022-05-04 20:07:09 -0400

    Add function for sticky header when scrolling

---
+++ index.js
@@ -0,0 +1,15 @@
+/* hide header when scrolling down */
+
+let prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (currentScrollPos > 100) {
+    if (prevScrollPos > currentScrollPos) {
+        document.querySelector("header").style.top = "0";
+    } else {
+        document.querySelector("header").style.top = "-200px";
+    }
+  }
+  console.log(prevScrollPos)
+  prevScrollPos = currentScrollPos;
+}
