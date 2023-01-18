a9e7978ac34a053397e40f1590131bd712c8d6e3
brandon <srlguitarist@gmail.com>
2023-01-18 00:14:57 -0500

    Edit to include throttling on window scroll

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -1,14 +1,22 @@
-/* hide header when scrolling down */
+import debounce from "./debounce.js";
 
+/* hide header when scrolling down */
 let prevScrollPos = window.pageYOffset;
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  if (currentScrollPos > 100) {
-    if (prevScrollPos > currentScrollPos) {
-      document.querySelector("header").style.top = "0";
-    } else {
-      document.querySelector("header").style.top = "-200px";
-    }
-  }
-  prevScrollPos = currentScrollPos;
-};
+window.addEventListener(
+  "scroll",
+  debounce(
+    () => {
+      let currentScrollPos = window.pageYOffset;
+      if (currentScrollPos > 100) {
+        if (prevScrollPos > currentScrollPos) {
+          document.querySelector("header").style.top = "0";
+        } else {
+          document.querySelector("header").style.top = "-200px";
+        }
+      }
+      prevScrollPos = currentScrollPos;
+    },
+    20,
+    { leading: true, maxWait: 20, trailing: true }
+  )
+);
